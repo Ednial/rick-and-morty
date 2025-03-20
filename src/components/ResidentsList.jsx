1f34038873d3482db183bf83fadd21db7adb4234
Ednial <96548579+Ednial@users.noreply.github.com>
2025-03-19 19:29:19 -0500

    modified/features-added

--- src/components/ResidentsList.jsx
+++ src/components/ResidentsList.jsx
@@ -4,11 +4,16 @@ import './ResidentsList.css';
 
 function ResidentsList({ residents }) {
 	return (
-		<div className="residents">
-			{residents.map((resident) => (
-				<ResidentCard key={resident} url={resident} />
-			))}
-		</div>
+		<>
+			<div className="residents">
+				{residents.map((resident) => (
+					<ResidentCard key={resident} url={resident} />
+				))}
+			</div>
+			{residents.length === 0 && (
+				<h2 className="residents__empty">No residents found</h2>
+			)}
+		</>
 	);
 }
 
